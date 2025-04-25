bc3dd66f69311fe0bc97287556d1eadcf3f3509d
Yusuke Kobayashi <yusukeko@usc.edu>
2025-04-25 05:44:43 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,8 +21,11 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, 
+DailySchedule& sched, vector<size_t>& workerShifts, size_t day, size_t slot);
 
-
+bool isValid(const AvailabilityMatrix& avail, const size_t maxShifts, const DailySchedule& sched,
+const vector<size_t>& workerShifts, size_t day, size_t slot, Worker_T workerId);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -37,9 +40,65 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+
+vector<size_t> workerShifts(avail[0].size(), 0);
+
+return scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, 0, 0);
 
 
 
+}
 
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched,
+vector<size_t>& workerShifts, size_t day, size_t slot){
+
+    if(day >= avail.size()){
+        return true;
+    }
+
+    if(slot>=dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, day+1, 0);
+    }
+
+    for(Worker_T workerId=0; workerId < avail[0].size(); workerId++){
+        if(isValid(avail, maxShifts, sched, workerShifts, day, slot, workerId)){
+            sched[day][slot] = workerId;
+            workerShifts[workerId]++;
+
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, day, slot+1)){
+                return true;
+            }
+
+            sched[day][slot] = INVALID_ID;
+            workerShifts[workerId]--;
+        }
+    }
+
+    return false;
+}
+
+bool isValid(const AvailabilityMatrix& avail, const size_t maxShifts, const DailySchedule& sched, const vector<size_t>& workerShifts,
+size_t day, size_t slot, Worker_T workerId){
+
+
+
+    if(!avail[day][workerId]){
+        
+        return false;
+    }
+
+    for(size_t i=0; i<slot; i++){
+
+        if(sched[day][i]==workerId){
+            return false;
+        }
+    }
+
+    if(workerShifts[workerId] >= maxShifts){
+        return false;
+    }
+
+    return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(string& candidate, const string& in, string remaining_floating, size_t pos, const set<string>& dict, set<string>& results);
+
 
 
 // Definition of primary wordle function
@@ -22,7 +24,76 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> results;
+    string candidate = in;
+    wordleHelper(candidate, in, floating, 0, dict, results);
+
+    return results;
 
 }
 
 // Define any helper functions here
+void wordleHelper(string& candidate, const string& in, string remaining_floating, size_t pos, const set<string>& dict, set<string>& results){
+
+    if(pos == in.length()){
+        if(remaining_floating.empty()){
+
+             if(dict.find(candidate)!=dict.end()){
+                results.insert(candidate);
+                }
+
+        return;
+
+        }
+    }
+       
+
+    if(in[pos]!='-'){
+        candidate[pos] = in[pos];
+        wordleHelper(candidate, in, remaining_floating, pos+1, dict, results);
+        return;
+    }
+
+    size_t remaining_blanks = 0;
+    for(size_t i=pos; i<in.length(); i++){
+
+        if(in[i] == '-'){
+            remaining_blanks++;
+        }
+    }
+
+    for(size_t i=0; i<remaining_floating.length(); i++){
+
+
+        char floatChar = remaining_floating[i];
+        candidate[pos] = floatChar;
+
+        string new_remaining = remaining_floating;
+        new_remaining.erase(i,1);
+
+        wordleHelper(candidate, in, new_remaining, pos+1, dict, results);
+
+    }
+
+    if(remaining_blanks>remaining_floating.length()){
+
+        for(char c='a'; c<='z'; c++){
+
+            bool skip = false;
+            for(size_t j =-0; j<remaining_floating.length(); j++){
+
+                if(c==remaining_floating[j]){
+
+                    skip = true;
+                    break;
+                }
+            }
+
+            if(!skip){
+                candidate[pos] = c;
+                wordleHelper(candidate, in, remaining_floating, pos+1, dict, results);
+            }
+        }
+    }
+
+    }
